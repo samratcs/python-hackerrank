e0406ca0fd85a388565d11ce3073d343a1de7f01
Samrat Sur <samrat.cs@outlook.com>
2026-07-19 20:44:07 +0530

    Solved Python If-Else

--- 01_introduction/02_python_if_else.py
+++ 01_introduction/02_python_if_else.py
@@ -20,3 +20,25 @@ Print Weird if the number is weird. Otherwise, print Not Weird.
 
 """
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n>=1 and n<=100:
+        if n%2 ==0:
+            if n>=2 and n<=5:
+                print("Not Weird")
+            elif n>=6 and n<=20:
+                print("Weird")
+            else:
+                print("Not Weird")
+        else:
+            print("Weird")
