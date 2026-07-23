b06dd51d72e62353406ff88191ede574db649660
Samrat Sur <samrat.cs@outlook.com>
2026-07-24 00:16:11 +0530

    feat: solve Runner Up Problem

---
+++ 02_basic_data_types/02_runner_up_score.py
@@ -0,0 +1,28 @@
+"""
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
+You are given  scores. Store them in a list and find the score of the runner-up.
+
+Input Format
+
+The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+
+Constraints
+. 2<=n<=10 and -100 <= n <=+ 100
+
+Output Format
+
+5
+Print the runner-up score.
+[2,3,6,6,5]
+
+Sample Input 0
+
+5
+2 3 6 6 5
+Sample Output 0
+
+5
+Explanation 0
+
+Given list is . The maximum score is , second maximum is . Hence, we print  as the runner-up score.
+"""
